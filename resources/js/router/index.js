cf08b0e27a9dfa01e470579c6e80cb3b243282d3
mzero <myintzaw2010@gmail.com>
2023-05-29 10:21:56 +0630

    laravel vue3

---
+++ resources/js/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import invoiceIndex from "../components/invoice/index.vue";
+import notFound from "../components/NotFound.vue";
+import invoiceNew from "../components/invoice/new.vue";
+
+const routes = [
+    {
+        path: "/",
+        component: invoiceIndex,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        component: notFound,
+    },
+    {
+        path: "/invoice/new",
+        component: invoiceNew,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
